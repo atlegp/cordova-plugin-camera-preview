573cdf0ede46f3bc572eb7999c1c1057616d3486
atlegp <atlegp@gmail.com>
2016-12-16 19:19:10 -0200

    Update CameraActivity.java

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -431,7 +431,7 @@ public class CameraActivity extends Fragment {
                     }
 
                     ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                    picture.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+                    picture.compress(Bitmap.CompressFormat.JPEG, 20, byteArrayOutputStream);
                     byte[] byteArray = byteArrayOutputStream.toByteArray();
 
                     String encodedImage = Base64.encodeToString(byteArray, Base64.NO_WRAP);
